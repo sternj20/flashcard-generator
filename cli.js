ae2e61385b7fb00f3069d141212b8d882e5eb7d6
sternj20 <jessesternmusic@gmail.com>
2017-07-19 13:22:21 -0700

    added recursive function createbasicquestion to create 5 basic questions, need to add for cloze and start appending questions into a json file

--- cli.js
+++ cli.js
@@ -1,35 +1,85 @@
 var BasicCard = require("./basiccard.js");
 var ClozeCard = require("./clozecard.js");
-
 var inquirer = require('inquirer');
 
-inquirer.prompt([
-	{
-		type:'list',
-		name: 'cardType',
-		message: 'Choose what kind of flashcard you want to make',
-		choices: ['Basic','Cloze']
-	},
-	{
-		type:'input',
-		name: 'fullQuestion',
-		message: 'Type in the question'
-	},
-	{
-		type:'input',
-		name: 'answer',
-		message: 'Type in the answer'
+//arrays to store users selected questions 
+var basicQuestions = [];
+var clozeQuestions = [];
+
+var count = 0;
+
+var basicCardQuestions = 	[{
+	type:'input',
+	name: 'basicQuestion',
+	message: 'Type in the question'
+},
+{
+	type:'input',
+	name: 'answer',
+	message: 'Type in the answer'
+}
+];
+
+var initialPrompt = {
+	type:'list',
+	name:'quizOrCreate',
+	message: 'Do you want to quiz yourself, or create a new set of flashcards?',
+	choices: ['Quiz', 'Create New']
+};
+var flashcardTypeQuestion = 	{
+	type:'list',
+	name: 'cardType',
+	message: 'Choose what kind of flashcard you want to make',
+	choices: ['Basic','Cloze']
+};
+
+
+var clozeCardQuestions = 	[{
+	type:'input',
+	name: 'clozeQuestion',
+	message: 'Type in the full question with the answer'
+},
+{
+	type:'input',
+	name: 'answer',
+	message: 'Type in the answer'
+}
+];
+
+//recursive function that creates 5 questions
+function createBasicQuestion(){
+	if(count < 5){
+		inquirer.prompt(basicCardQuestions).then(function(answers){
+			var newBasicCard = new BasicCard(answers.basicQuestion, answers.answer);
+			console.log(newBasicCard);
+			basicQuestions.push(newBasicCard);
+			console.log(basicQuestions);
+			createBasicQuestion();
+		});
+		count++;
 	}
-])
+}
 
-.then(function (answers) {
+//ask if the user wants to be quizzed or wants to create flash cards to quiz themselves on
+inquirer.prompt(initialPrompt).then(function(answers){
+	if(answers.quizOrCreate === 'Create New'){
+		//creating flash cards
 
-	console.log(answers.cardType);
-	if(answers.cardType === 'Basic'){
-		var newBasicCard = new BasicCard(answers.fullQuestion, answers.answer);
-		console.log(newBasicCard);
-	} else{
-		var newClozeCard = new ClozeCard(answers.fullQuestion, answers.answer);
-		console.log(newClozeCard);
+		inquirer.prompt(flashcardTypeQuestion).then(function(answers){
+			if (answers.cardType === 'Basic'){
+				createBasicQuestion();
+			} else {
+				inquirer.prompt(clozeCardQuestions).then(function(answers){
+					var newClozeCard = new ClozeCard(answers.clozeQuestion, answers.answer);
+					console.log(newClozeCard);
+					console.log(answers);
+				});
+			}
+		});
+	} else {
+		//quizzing from existing flashcards
 	}
 });
+
+
+
--- clozecard.js
+++ clozecard.js
@@ -2,7 +2,6 @@ var ClozeCard = function(text, cloze){
 	if(text.indexOf(cloze) === -1) {
 		console.log ('you need to have the cloze in your text');
 	} else {
-		this.text = text;
 		this.cloze = cloze;
 		this.partial = text.replace(cloze,'');
 		this.fulltext = text;
